ec69982d08d358dbcca6dad11282a4232c466204
LuizGmes <luiz.gmes@gmail.com>
2019-03-13 20:59:34 -0300

    Estado e Cidade

--- src/main/java/com/gomesluiz/cursomc/domain/Estado.java
+++ src/main/java/com/gomesluiz/cursomc/domain/Estado.java
@@ -8,13 +8,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import javax.persistence.OneToMany;
 
 
 @Entity
-public class Categoria implements Serializable {
+public class Estado implements Serializable {
+	
 	private static final long serialVersionUID = 1L;
 	
 	@Id
@@ -22,14 +21,13 @@ public class Categoria implements Serializable {
 	private Integer id;
 	private String nome;
 	
-	@JsonManagedReference
-	@ManyToMany(mappedBy="categorias")
-	private List<Produto> produtos = new ArrayList<>();
+	@OneToMany(mappedBy="estado")
+	private List<Cidade> cidades = new ArrayList<>();
 	
-	public Categoria(){	
+	public Estado(){	
 	}
 
-	public Categoria(Integer id, String nome) {
+	public Estado(Integer id, String nome) {
 		super();
 		this.id = id;
 		this.nome = nome;
@@ -51,12 +49,12 @@ public class Categoria implements Serializable {
 		this.nome = nome;
 	}
 	
-	public List<Produto> getProdutos() {
-		return produtos;
+	public List<Cidade> getCidades() {
+		return cidades;
 	}
 
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
+	public void setCidade(List<Cidade> cidades) {
+		this.cidades = cidades;
 	}
 
 	@Override
@@ -75,7 +73,7 @@ public class Categoria implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Estado other = (Estado) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
