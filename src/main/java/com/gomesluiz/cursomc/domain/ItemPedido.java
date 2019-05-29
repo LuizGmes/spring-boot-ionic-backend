8a08c11cb3e690acc0ca0e11a996a0ab5e8150f8
LuizGmes <luiz.gmes@gmail.com>
2019-05-28 22:27:26 -0300

    Implementando toString de Pedido

--- src/main/java/com/gomesluiz/cursomc/domain/ItemPedido.java
+++ src/main/java/com/gomesluiz/cursomc/domain/ItemPedido.java
@@ -1,6 +1,8 @@
 package com.gomesluiz.cursomc.domain;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
@@ -30,7 +32,7 @@ public class ItemPedido implements Serializable {
 		this.quantidade = quantidade;
 		this.preco = preco;
 	}
-	
+
 	public double getSubTotal() {
 		return (preco - desconto) * quantidade;
 	}
@@ -109,6 +111,19 @@ public class ItemPedido implements Serializable {
 		return true;
 	}
 	
-	
+	@Override
+	public String toString() {
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt","BR"));
+		StringBuilder builder = new StringBuilder();
+		builder.append(getProduto().getNome());
+		builder.append(", Qte: ");
+		builder.append(getQuantidade());
+		builder.append(", Preço Unitário: ");
+		builder.append(nf.format(getPreco()));
+		builder.append(", Subtotal: ");
+		builder.append(nf.format(getSubTotal()));
+		builder.append("\n");
+		return builder.toString();
+	}
 
 }
